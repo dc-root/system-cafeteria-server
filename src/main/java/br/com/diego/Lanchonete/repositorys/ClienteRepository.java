b11c33b4e08781501eee9daea7ad93ac2ab71bb6
Diego C. Silva <diegocaetano444@outlook.com>
2022-10-11 22:39:17 -0300

    POO: added cliente interface

--- src/main/java/br/com/diego/Lanchonete/repositorys/ClienteRepository.java
+++ src/main/java/br/com/diego/Lanchonete/repositorys/ClienteRepository.java
@@ -1,7 +1,29 @@
 package br.com.diego.Lanchonete.repositorys;
 
 import br.com.diego.Lanchonete.repositorys.interfaces.ClienteRepositoryInterface;
+import br.com.diego.Lanchonete.modelos.Cliente;
 
-public class ClienteRepository implements ClienteRepositoryInterface {
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public abstract class ClienteRepository implements ClienteRepositoryInterface {
+    private Cliente cliente = new Cliente();
+
+    public List<Cliente> listar() {
+        
+    }
     
+    public Cliente buscar(Long id) {
+
+    }
+
+    public Cliente salvar(Cliente cliente) {
+        
+    }
+    
+    public void remover(Cliente cliente) {
+
+    }
 }
--- src/main/java/br/com/diego/Lanchonete/repositorys/interfaces/ClienteRepositoryInterface.java
+++ src/main/java/br/com/diego/Lanchonete/repositorys/interfaces/ClienteRepositoryInterface.java
@@ -1,5 +1,12 @@
 package br.com.diego.Lanchonete.repositorys.interfaces;
 
+import br.com.diego.Lanchonete.modelos.Cliente;
+
+import java.util.List;
+
 public interface ClienteRepositoryInterface {
-    
+    List<Cliente> listar();
+    Cliente buscar(Long id);
+    Cliente salvar(Cliente cliente);
+    void remover(Cliente cliente);
 }
